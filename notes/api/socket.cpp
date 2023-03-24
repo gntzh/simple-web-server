beea02ddecb78d5da1d7b56ab2c2a1c8b362eedf
Grant Zhang <hi@gntzh.com>
2023-03-24 22:05:04 +0800

    notes: 笔记

--- notes/api/socket.cpp
+++ notes/api/socket.cpp
@@ -5,6 +5,7 @@
 int socket(int domain, int type, int protocol); // 文件描述符
 int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
 int listen(int fd, int n);
+// n 是指已连接但未进行 accept 处理的SOCKET，是SYN队列和ACCEPT队列之和
 
 // 等待连接
 int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen); // 文件描述符
