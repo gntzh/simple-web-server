a6b967d3078973c2c7040bd281dec356ba8a9e1a
Grant Zhang <hi@gntzh.com>
2023-03-24 12:49:06 +0800

    chore: 重置 mian.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,6 @@
-#include <algorithm>
-#include <chrono>
 #include <iostream>
-#include <mutex>
-#include <ostream>
-#include <thread>
-#include <vector>
-
-std::mutex cout_mutex;
-
-void foo(int num) {
-  cout_mutex.lock();
-  std::cout << ">>> "
-            << "Thread: " << num << " (" << std::this_thread::get_id() << ")"
-            << std::endl;
-  cout_mutex.unlock();
-  std::this_thread::sleep_for(std::chrono::seconds{std::min(1, 8 - num)});
-  cout_mutex.lock();
-  std::cout << "Thread: " << num << " <<< " << std::endl;
-  cout_mutex.unlock();
-}
 
 int main(int argc, char **argv) {
-  const int thread_num = 5;
-  std::vector<std::thread> threads;
-
-  threads.reserve(thread_num);
-  for (int i = 0; i < thread_num; ++i) {
-    threads.emplace_back(foo, i);
-  }
-  for (auto &t : threads) {
-    t.join();
-  }
-  std::cout << "work in main thread" << std::endl;
-  std::cout << "" << std::endl;
+  std::cout << "Hello, world!" << std::endl;
   return 0;
 }
