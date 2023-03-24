6b560f218b80ff02d683cb7251afcf3447a4a5e5
Grant Zhang <hi@gntzh.com>
2023-03-24 22:07:04 +0800

    chore: 重置main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,6 @@
-#include "thread_pool/thread_pool.h"
 #include <iostream>
-#include <ostream>
-#include <string>
 
-int main() {
-  thread_pool::ThreadPool pool{10};
-  for (int i = 0; i < 10; ++i) {
-    pool.execute([i] {
-      std::string s = "Thread: ";
-      s += std::to_string(i);
-      s += "\n";
-      std::cout << s << std::flush;
-    });
-  }
+int main(int argc, char **argv) {
+  std::cout << "Hello, world!" << std::endl;
   return 0;
 }
